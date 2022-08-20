de7f62f34a3b97354330c142853c85384c10611a
Pattaraporn Hanphadungkit <pataraporn.han@gmail.com>
2022-08-20 04:24:52 +0000

    get stock peice

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import yfinance as yf
 import datetime as dt
 import string
 import config
+import utils
 
 def get_pubsub_message(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -32,6 +33,24 @@ def get_list_of_stock_symbol():
         symbol_df = pd.read_html(str(symbol))[0]
         all_symbol.append(symbol_df)
         time.sleep(0.5)
+    all_symbol = pd.concat(all_symbol)
     return all_symbol
 
+
+def get_stock_price(event, context):
+    
+    messages = get_pubsub_message(event, context)
+    if messages == "monthly update stock":
+        pass
+    else:
+        raise Exception(f"{messages} messages does not support")
+    
+    current = dt.datetime.now()
+    prev_7d = current - dt.timedelta(days=7)
+    all_symbol = get_list_of_stock_symbol()
+    yf_ticker = [f"{s}.BK" for s in all_symbol['Symbol'].values]
+    yf_ticker = ' '.join(yf_ticker)
+    history = yf.download(tickers=yf_ticker,start=prev_7d)
+    
+    
 

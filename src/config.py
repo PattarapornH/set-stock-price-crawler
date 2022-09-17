d2f54af9d7b11092e2c50bb399a48d02ddf2b02d
Pattaraporn Hanphadungkit <pataraporn.han@gmail.com>
2022-09-17 12:20:03 +0700

    delete stock table

--- src/config.py
+++ src/config.py
@@ -3,4 +3,3 @@ import os
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
 BQ_DATASET = os.environ.get("BQ_DATASET")
 BQ_PRICE_TABLE_NAME = os.environ.get("BQ_PRICE_TABLE_NAME")
-BQ_STOCK_TABLE_NAME = os.environ.get("BQ_STOCK_TABLE_NAME")
--- src/gcf_deploy.sh
+++ src/gcf_deploy.sh
@@ -1,9 +1,8 @@
 echo "Deploy to project $GCP_PROJECT"
 echo "Deploy to region $GCP_FUNCTION_REGION"
 echo "Triggered by $GCP_TOPIC_STOCK_CRAWLING topic"
-echo "BQ daataset name $BQ_DATASET"
+echo "BQ dataset name $BQ_DATASET"
 echo "BQ price table name $BQ_PRICE_TABLE_NAME"
-echo "BQ stock table name $BQ_STOCK_TABLE_NAME"
 
 gcloud functions deploy set-crawler \
     --entry-point main \
@@ -17,5 +16,4 @@ gcloud functions deploy set-crawler \
     --set-env-vars="GCP_PROJECT"=$GCP_PROJECT \
     --set-env-vars="BQ_DATASET"=$BQ_DATASET \
     --set-env-vars="BQ_PRICE_TABLE_NAME"=$BQ_PRICE_TABLE_NAME \
-    --set-env-vars="BQ_STOCK_TABLE_NAME"=$BQ_STOCK_TABLE_NAME \
     --timeout 540s
